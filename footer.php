484a9870b056397c1582b3c74898f0cfe4d4f2f0
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-05-09 18:00:12 +0200

    Ajout du fichier footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main>
+
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
