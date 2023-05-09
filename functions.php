a9f91b1bbc8a6c34fcdb374a447870e79eb3b316
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-05-09 17:45:21 +0200

    Ajout des menus header et footer

--- functions.php
+++ functions.php
@@ -3,5 +3,17 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css');
 }
+
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Header Menu' ),
+        'footer-menu' => __( 'Footer Menu' )
+      )
+    );
+}
+add_action( 'init', 'register_my_menus' );
+add_theme_support( 'custom-logo' );
