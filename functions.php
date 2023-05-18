4a0b935292f449e17d018dcbb242b3377ba95408
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-05-18 20:20:19 +0200

    MAJ code function menu

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css');
 }
 
-function register_my_menus() {
+function register_nav_menus() {
     register_nav_menus(
       array(
         'header-menu' => __( 'Header Menu' ),
@@ -15,7 +15,7 @@ function register_my_menus() {
       )
     );
 }
-add_action( 'init', 'register_my_menus' );
+add_action( 'init', 'register_nav_menus' );
 add_theme_support( 'custom-logo' );
 
 
